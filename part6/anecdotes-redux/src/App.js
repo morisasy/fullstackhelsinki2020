119c91e65060240d2c83ceb21635b1279923b66a
Medi Risasi <morisasy@gmail.com>
2020-06-02 21:29:44 +0300

    6.19 anecdotes and connect, step1

--- part6/anecdotes-redux/src/App.js
+++ part6/anecdotes-redux/src/App.js
@@ -1,11 +1,16 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { connect } from 'react-redux'
+import { initializeAnecdotes } from './reducers/anecdoteReducer';
 import AnecdoteForm from './components/AnecdoteForm'
 import AnecdoteList from './components/AnecdoteList'
 import Notification from './components/Notification'
 import Filter from './components/Filter'
 
 
-const App = () => {
+const App = (props) => {
+  useEffect(() => {
+    props.initializeAnecdotes()
+  }, [])
  
   return (
     <div>
@@ -18,4 +23,5 @@ const App = () => {
   )
 }
 
-export default App
+//export default App
+export default connect(null, { initializeAnecdotes })(App)
